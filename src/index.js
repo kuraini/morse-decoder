9c10cbf829425a63781255b46cceebac2c770b7d
Svetlana Borisova <kuraini@yandex.ru>
2022-02-12 21:43:49 +0300

    add a solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,60 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const arr = expr.split('');
+    const arr2 = [];
+    const arr3 = [];
+    const arr4 = [];
+    const arr5 = [];
+
+    for (let i = 0 ; arr.length > 0; i++) {
+      arr2[i] = arr.splice(0, 10);
+    }
+
+    for (let item of arr2) {
+      let copy = item.slice(0);
+      let arr = [];
+    
+      for (let j = 0; copy.length > 0; j++) {
+        arr[j] = copy.splice(0, 2).join('');
+      }
+      
+      arr3.push(arr);
+    }
+
+    for (let item of arr3) {
+      let str = '';
+      for (let i of item) {
+        if (i === '10') {
+          str += '.';
+        } else if (i === '11') {
+          str += '-';
+        } else if (i === '**') {
+          str += ' ';
+        }
+      }
+      
+      arr4.push(str);
+    }
+
+    for (let item of arr4) {
+      let arr = [];
+      if (item.includes(' ')) {
+        let str = item.replace(/\s+/g,'').trim();
+        let symb = str.length > 0 ? MORSE_TABLE[str] : ' ';
+        if (symb !== ' ') {
+          arr.push(' ' + symb);
+        } else {
+          arr.push(symb);
+        }
+      } else if (MORSE_TABLE[item]) {
+        arr.push(MORSE_TABLE[item]);
+      }
+
+      arr5.push(arr);
+    }
+
+    return arr5.join('');
 }
 
 module.exports = {
